f20f20b45f79779e2bb9353e8549d1fbb7bfab92
Legion-fire <Legion-fire@yandex.ru>
2022-12-15 00:27:02 +0300

    Исправил установку значения по умолчанию.

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarServiseImpl;
 
-import java.util.Optional;
-
 @Controller
 public class CarController {
 
@@ -18,8 +16,8 @@ public class CarController {
     }
 
     @GetMapping("cars")
-    public String printCar(@RequestParam(value = "count", defaultValue = "5") Optional<Integer> count, Model model){
-        model.addAttribute("cars", carServise.carListCount(count.orElse(0)));
+    public String printCar(@RequestParam(value = "count", defaultValue = "5") int count,  Model model){
+        model.addAttribute("cars", carServise.carListCount(count));
         return "cars";
     }
 
